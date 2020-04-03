5e7712be1830494ba0d43f920447a61e39fd9494
Sohail <haqyarsohail@gmail.com>
2020-04-03 21:42:54 +0300

    Added the card for each ngo

--- client-side/src/components/pages/Listboard.js
+++ client-side/src/components/pages/Listboard.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
+import OrganizationCard from "../OrganizationCard";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
-
 export class Listboard extends Component {
   render() {
     return (
-      <div>
+      <div className="">
         <Breadcrumb>
           <BreadcrumbItem>
             <Link to="/home">Home</Link>
           </BreadcrumbItem>
           <BreadcrumbItem active>Listboard</BreadcrumbItem>
         </Breadcrumb>
-        <p>
-          scroll on our registered NGOs since you have refused to register, see
-          your friend growing higher while you are dying with corona. enjoy
-          before we say bye bye.
-        </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">
+                Organizations to Checkout
+              </h1>
+              <p className="lead text-center">
+                Browse and connect with other NGO's
+              </p>
+              <OrganizationCard />
+              <OrganizationCard />
+              <OrganizationCard />
+              <OrganizationCard />
+              <OrganizationCard />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
