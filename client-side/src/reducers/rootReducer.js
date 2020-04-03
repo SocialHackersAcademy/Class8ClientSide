b85522e65c807e81c6b38d5807ab9b4107133d55
Sohail <haqyarsohail@gmail.com>
2020-04-03 23:09:34 +0300

    Aded dynamic a dynamic card component with redux

--- client-side/src/reducers/rootReducer.js
+++ client-side/src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
-
+import organizationsReducer from "./organizationsReducer";
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  ngo: organizationsReducer
 });
