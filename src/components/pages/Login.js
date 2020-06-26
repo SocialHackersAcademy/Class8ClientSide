7fd68669c001a8da93ee5cd1fe76c0a4c93d36c0
Sohail Haqyar <haqyarsohail@gmail.com>
2020-06-26 18:25:24 +0300

    added git ignore

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,73 +1,108 @@
-import React, { Component } from "react";
-import * as bootstrap from "bootstrap"
-import * as reactstrap from  "reactstrap"
-import {loginUser} from "../../actions/authActions";
+import React, { Component } from 'react';
+import * as bootstrap from 'bootstrap';
+import * as reactstrap from 'reactstrap';
+import { loginUser } from '../../actions/authActions';
 
 export class Login extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {message:'', messageType:'invisible'};
+    this.state = { message: '', messageType: 'invisible' };
   }
   handleSubmit(event) {
     event.preventDefault();
-    let email = event.target.querySelector("#email");
-    let password = event.target.querySelector("#password");
-    let rememberMe = event.target.querySelector("#rememberMe");
+    let email = event.target.querySelector('#email');
+    let password = event.target.querySelector('#password');
+    let rememberMe = event.target.querySelector('#rememberMe');
     if (email != null && password != null) {
-        if (loginUser("?username="+email+"&password="+password, rememberMe)) {
-            this.setState({ message:'You are logged in', messageType:'alert alert-primary' });
-        }
+      if (
+        loginUser('?username=' + email + '&password=' + password, rememberMe)
+      ) {
+        this.setState({
+          message: 'You are logged in',
+          messageType: 'alert alert-primary',
+        });
+      }
     } else {
-        this.setState({ message:'Please fill the fields', messageType:'alert alert-danger' });
+      this.setState({
+        message: 'Please fill the fields',
+        messageType: 'alert alert-danger',
+      });
     }
   }
   render() {
     return (
-     <div class="container">
-      <div class="row d-flex justify-content-center">
-         <div class="col-8 text-center">
-           <form onSubmit={(event)=>this.handleSubmit(event)}>
-                <br />
-                <br />
-                <h3>Log In</h3>
-                <br />
-                <br />
-                <div className="input-group">
-                    <div className="input-group-prepend">
-                        <label className="input-group-text" htmlFor="email">Email address</label>
-                    </div>
-                    <input type="email" id="email" className="form-control" placeholder="Enter email here" />
+      <div class='container'>
+        <div class='row d-flex justify-content-center'>
+          <div class='col-8 text-center'>
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+              <br />
+              <br />
+              <h3>Log In</h3>
+              <br />
+              <br />
+              <div className='input-group'>
+                <div className='input-group-prepend'>
+                  <label className='input-group-text' htmlFor='email'>
+                    Email address
+                  </label>
                 </div>
-                <br />
-                <div className="input-group">
-                    <div className="input-group-prepend">
-                       <label className="input-group-text" htmlFor="password">Password</label>
-                    </div>
-                    <input type="password" id="password" className="form-control" placeholder="Enter password here" />
+                <input
+                  type='email'
+                  id='email'
+                  className='form-control'
+                  placeholder='Enter email here'
+                />
+              </div>
+              <br />
+              <div className='input-group'>
+                <div className='input-group-prepend'>
+                  <label className='input-group-text' htmlFor='password'>
+                    Password
+                  </label>
                 </div>
-                <br />
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="rememberMe" />
-                        <label className="custom-control-label" htmlFor="rememberMe">Remember me</label>
-                    </div>
+                <input
+                  type='password'
+                  id='password'
+                  className='form-control'
+                  placeholder='Enter password here'
+                />
+              </div>
+              <br />
+              <div className='form-group'>
+                <div className='custom-control custom-checkbox'>
+                  <input
+                    type='checkbox'
+                    className='custom-control-input'
+                    id='rememberMe'
+                  />
+                  <label className='custom-control-label' htmlFor='rememberMe'>
+                    Remember me
+                  </label>
                 </div>
-                <br />
-                <button type="submit" className="btn btn-primary btn-block yolonet-submit">Submit</button>
-                <br />
-                <p className="forgot-password text-right">
-                    Forgot <a href="/passReset">password?</a>
-                </p>
+              </div>
+              <br />
+              <button
+                type='submit'
+                className='btn btn-primary btn-block yolonet-submit'
+              >
+                Submit
+              </button>
+              <br />
+              <p className='forgot-password text-right'>
+                Forgot <a href='/passReset'>password?</a>
+              </p>
             </form>
             <br />
             <br />
-            <div class={this.state.messageType} role="alert">{this.state.message}</div>
+            <div class={this.state.messageType} role='alert'>
+              {this.state.message}
+            </div>
             <br />
             <br />
+          </div>
         </div>
       </div>
-     </div>
     );
   }
 }
